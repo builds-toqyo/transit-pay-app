c10f3483fa9872762035e264b4a9a5b76c61b1ad
JosephMugo8711 <wamiti8711@gmail.com>
2024-05-18 11:51:18 +0300

    Feat: homepage 🚀

--- app/(stack)/(drawer)/(bottomtabs)/(toptabs)/_layout.tsx
+++ app/(stack)/(drawer)/(bottomtabs)/(toptabs)/_layout.tsx
@@ -7,6 +7,7 @@ import {
 } from "@react-navigation/material-top-tabs";
 import { withLayoutContext } from "expo-router";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -18,9 +19,44 @@ export const MaterialTopTabs = withLayoutContext<
 >(Navigator);
 const RootLayout = () => {
   return (
-    <View className="dark:bg-dark bg-white flex-1">
-      <Text className="dark:text-white text-dark"> this is home page </Text>
+    <View className="flex-1 bg-white">
+    <View className="flex-row items-center mx-12 my-9 bg-[#1F41BB] justify-between px-12 py-12 rounded-2xl">
+      <View className="flex">
+        <Text className="text-lg font-bold text-white">Balance</Text>
+        <Text className="text-xl text-white">Ksh 1000</Text>
+      </View>
+      <Ionicons name="add-circle-outline" size={32} color="white" />
     </View>
+    <View className="flex-row justify-around px-4 py-2 bg-gray-200">
+      <Text className="text-base">Transfer</Text>
+      <Text className="text-base">Payment</Text>
+      <Text className="text-base">Payout</Text>
+      <Text className="text-base">Top Up</Text>
+    </View>
+    <View className="px-4 py-5">
+      <View className="flex-row justify-between">
+        <Text className="mb-2 text-lg font-bold">Last Transaction</Text>
+        <Text className="text-base text-right text-gray-400">View All</Text>
+      </View>
+      <View className="mt-4">
+        <View className="flex-row items-center justify-between mb-2">
+          <Text className="text-lg text-gray-400">N</Text>
+          <Text className="text-lg">Kenya Airways</Text>
+          <Text className="text-lg">$12.00</Text>
+        </View>
+        <View className="flex-row items-center justify-between mb-2">
+          <Text className="text-lg text-gray-400">P</Text>
+          <Text className="text-lg">Nairobi Terminers</Text>
+          <Text className="text-lg">$10.00</Text>
+        </View>
+        <View className="flex-row items-center justify-between mb-2">
+          <Text className="text-lg text-gray-400"></Text>
+          <Text className="text-lg">Supermetro</Text>
+          <Text className="text-lg">Ksh 52.00</Text>
+        </View>
+      </View>
+    </View>
+  </View>
   );
 };
 
